0431c7fab18b453d7f0aac876ce9a55886e3d672
Mateus Alcova <mateusalcova@gmail.com>
2021-10-19 18:52:06 -0300

    chore: temporary fix on instagram

--- src/pages/api/instagram-web-api.js
+++ src/pages/api/instagram-web-api.js
@@ -13,14 +13,14 @@ const handler = nc().get(async (req, res) => {
   let response = await fetch(req);
   response = new Response(response.body, response);
 
-  const domain =
-    // eslint-disable-next-line no-undef
-    process.env.NODE_ENV === 'production'
-      ? 'https://biodiversidadeurbana.com.br/'
-      : 'http://localhost:3000/';
+  // const domain =
+  //   // eslint-disable-next-line no-undef
+  //   process.env.NODE_ENV === 'production'
+  //     ? 'https://biodiversidadeurbana.com.br'
+  //     : 'http://localhost:3000';
 
   response.headers.set('Cross-Origin-Resource-Policy', 'cross-origin');
-  response.headers.set('Access-Control-Allow-Origin', domain);
+  // response.headers.set('Access-Control-Allow-Origin', domain);
   response.headers.append('Vary', 'Origin');
   return res.send(response.body);
 });
